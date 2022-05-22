acd33a9990b745b4f4b2d8da11cf4798b21d1a87
Alberto Battistel <alberto.battistel@rub.de>
2022-05-22 20:26:29 +0200

    Finished the main finite state machine.

--- tower/main.py
+++ tower/main.py
@@ -53,27 +53,54 @@ class TowerGame:
 	screen: pygame.Surface
 	screen_rect: pygame.Rect
 	fullscreen: bool
-	 
+	state: GameState
+	
+	
 	@classmethod
 	def create(cls, fullscreen=False):
 		game = cls(
 			screen=None,
 			screen_rect=SCREENRECT,
 			fullscreen=fullscreen,
+			state=GameState.initializing,
 			)
 		game.init()
 		return game
 		
+	def set_state(self, new_state):
+		self.state = new_state
+		
+	def assert_state_is(self, *expected_states:GameState):
+		"""
+		Asserts that the game engine is one of
+		'expected_states'. If that assertion fails, raise
+		'StateError'
+		"""
+		if not self.state in expected_states:
+			raise StateError(
+				f"Expected the game state to be one of {expected_states} not {self.state}"
+				)
+		
+	def start_game(self):
+		self.assert_state_is(GameState.initialized)
+		self.set_state(GameState.main_menu)
+		self.loop()
+	
 	def loop(self):
-		pass
+		while self.state != GameState.quitting:
+			if self.state == GameState.main_menu:
+				# pass control to the game menu's loop
+			elif self.state == GameState.map_editing:
+				# ...etc...
+			elif self.state == GameState.game_playing:
+				# ...etc...
+		self.quit()
 		
 	def quit(self):
 		pygame.quit()
 		
-	def start_game(self):
-		self.loop()
-		
 	def init(self):
+		self.assert_state_is(GameState.initializing)
 		pygame.init()
 		window_style = pygame.FULLSCREEN if self.fullscreen else 0
 		# We want 32 bits of color depth
@@ -87,7 +114,7 @@ class TowerGame:
 			)
 		pygame.font.init()
 		self.screen = screen
-			
+		self.set_state(GameState.initialized)	
 
 def start_game():
     game = TowerGame.create()
