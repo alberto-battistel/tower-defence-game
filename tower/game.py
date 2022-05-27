83a7738c6211105bd0cab6da0db284e0a03d95c7
Alberto Battistel <alberto.battistel@rub.de>
2022-05-27 22:50:10 +0200

    Finished part "Basic Game Template".
    There were a lot of things left out in the tutorial and I needed to
    check the source files of the demo.

--- tower/game.py
+++ tower/game.py
@@ -6,9 +6,22 @@ from dataclasses import dataclass, field
 import enum
  
 import pygame
- 
-SCREENRECT = pygame.Rect(0,0,1024,800)
-MOUSE_LEFT, MOUSE_MIDDLE, MOUSE_RIGHT = 1, 2, 3
+
+from tower.constants import (
+	SCREENRECT,
+	MOUSE_LEFT, 
+	MOUSE_MIDDLE, 
+	MOUSE_RIGHT,
+	DESIRED_FPS,
+	SPRITES,
+	IMAGE_SPRITES,
+	SOUNDS,
+	SOUND_ESCAPED,
+	SOUND_FOOTSTEPS,
+	SOUND_TURRET,)
+
+from tower.loader import import_image, import_sound
+
 
 class GameState(enum.Enum):
 	"""
@@ -50,15 +63,26 @@ class TowerGame:
 	screen: pygame.Surface
 	screen_rect: pygame.Rect
 	fullscreen: bool
+	channels: dict
 	state: GameState
-	game_menu: GameLoop = field(init=False, default=None)
+	game_menu: "GameLoop" = field(init=False, default=None)
 	
 	@classmethod
 	def create(cls, fullscreen=False):
+		"""
+		Creates a TowerGame instance with sensible defaults.
+		"""
+		
+		channels = {
+			"footsteps": None,
+			"turrets": None,
+			"score": None,
+			}
 		game = cls(
 			screen=None,
 			screen_rect=SCREENRECT,
 			fullscreen=fullscreen,
+			channels=channels,
 			state=GameState.initializing,
 			)
 		game.init()
@@ -85,19 +109,44 @@ class TowerGame:
 	
 	def init(self):
 		self.assert_state_is(GameState.initializing)
+		# Initialize and configure the display and mode for the game
 		pygame.init()
 		window_style = pygame.FULLSCREEN if self.fullscreen else 0
-		# We want 32 bits of color depth
+		# Configure fullscreen or windowed, the color depth (32 bits) and create the screen surface
 		bit_depth = pygame.display.mode_ok(self.screen_rect.size, window_style, 32)
-		screen = pygame.display.set_mode(self.screen_rect.size, window_style, bit_depth)
+		self.screen = pygame.display.set_mode(self.screen_rect.size, window_style, bit_depth)
+		# Load the image tiles into the module-level dictionary 'IMAGE_SPRITES'
+		for sprite_index, sprite_name in SPRITES.items():
+			img = import_image(sprite_name)
+			for flipped_x in (True, False):
+				for flipped_y in (True,False):
+					new_img = pygame.transform.flip(img, flip_x=flipped_x, flip_y=flipped_y)
+					IMAGE_SPRITES[(flipped_x, flipped_y, sprite_index)] = new_img
+		# Configure the sound mixer.
 		pygame.mixer.pre_init(
 			frequency=44100,
 			size=32,
 			channels=2,
 			buffer=512,
 			)
+		if pygame.mixer.get_init() is None:
+			pygame.mixer = None
+		else:
+			# Load the sounds
+			for sound_key, sound_name in SOUNDS.items():
+				sound = import_sound(sound_name)
+				SOUNDS[sound_key] = sound
+
+			# Map the channels and channel names to a dedicated
+			# `Channel` object sourced from pygame's sound mixer.
+			for channel_id, channel_name in enumerate(self.channels):
+				self.channels[channel_name] = pygame.mixer.Channel(channel_id)
+				# Configure the volume here.
+				self.channels[channel_name].set_volume(1.0)
+			
+		# Load the font engine
 		pygame.font.init()
-		self.screen = screen
+		#self.screen = screen
 		self.game_menu = GameMenu(game=self)
 		self.set_state(GameState.initialized)
 		
@@ -105,10 +154,10 @@ class TowerGame:
 		while self.state != GameState.quitting:
 			if self.state == GameState.main_menu:
 				self.game_menu.loop()
-			elif self.state == GameState.map_editing:
-				# ...etc...
-			elif self.state == GameState.game_playing:
-				# ...etc...
+			# elif self.state == GameState.map_editing:
+				# # ...etc...
+			# elif self.state == GameState.game_playing:
+				# # ...etc...
 		self.quit()
 		
 	def quit(self):
@@ -127,8 +176,7 @@ class GameLoop:
 		game window to hang.
 		"""
 		for event in pygame.event.get():
-			if (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE) or
-				event.type == pygame.QUIT:
+			if (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE) or event.type == pygame.QUIT:
 				self.set_state(GameState.quitting)
 			# Delegates the event to a sub-event handler 'handle_event'
 			self.handle_event(event)
@@ -137,7 +185,7 @@ class GameLoop:
 		while self.state != GameState.quitting:
 			self.handle_events()
 			
-	def handle_event(self):
+	def handle_event(self,event):
 		"""
 		Handles a singular event.
 		"""
@@ -154,9 +202,16 @@ class GameLoop:
 	def state(self):
 		return self.game.state
 
-		
+@dataclass		
 class GameMenu(GameLoop):
-	pass
+	def loop(self):
+		clock = pygame.time.Clock()
+		self.screen.blit(IMAGE_SPRITES[(False, False, "backdrop")], (0,0))
+		while self.state == GameState.main_menu:
+			self.handle_events()
+			pygame.display.flip()
+			pygame.display.set_caption(f"FPS{round(clock.get_fps())}")
+			clock.tick(DESIRED_FPS)
 
 
 class GameEditing(GameLoop):
@@ -164,9 +219,9 @@ class GameEditing(GameLoop):
 	
 	
 def start_game():
-    game = TowerGame.create()
-    game.loop()
+	game = TowerGame.create()
+	game.start_game()
 
 if __name__ == '__main__':
-    start_game()
+	start_game()
 
